dfce6ac8400b8cc965a428b41bc6bbd24233e37d
Brian Kim <briankimpossible@gmail.com>
2015-08-27 15:22:28 -0400

    Discover that diagonal scrolling broken as shit in Chrome/Safari

--- scroll/index.js
+++ scroll/index.js
@@ -2,37 +2,62 @@ var $ = require('jquery');
 var React = require('react');
 var {maps} = require('google');
 
-function makeScrollAware(Component, container) {
+function getEmbeddedComponent(Component, [width, height], container) {
+
   class Spy extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        width: width,
+        height: height,
+      };
+    }
+
     render() {
-      var size = 10000;
+      var width = this.state.width || 10000;
+      var height = this.state.height || 10000;
       var initialStyle = {
-        width: `${size}%`,
-        height: `${size}%`,
-        top: `-${size/2}%`,
-        left: `-${size/2}%`,
+        width: `${width}`,
+        height: `${height}`,
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        overflow: 'scroll',
       };
       return (
-        <div style={initialStyle}>
-          <Component {...this.props} {...this.state} />
+        <div onScroll={this.handleScroll} style={initialStyle}>
+          <Component {...this.props} />
         </div>
       );
+    }
+
+    componentDidMount() {
+      var $container = $(container);
+      this.setState({
+        width: $container.width,
+        height: $container.height,
+      });
+      $(window).on('scroll', this.handleScroll);
+    }
+
+    handleScroll(ev) {
+      console.log([$(window).scrollLeft(), $(window).scrollTop()]);
     }
   }
   return Spy;
 }
 
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>Hello World</div>
+    );
+  }
+}
+
 $(function() {
   var initialWidth = $(document.body).width();
   var initialHeight = $(document.body).height();
-  $(window).on('scroll', () => {
-    console.log($(window).scrollTop());
-  });
-  $('.text-container').css({
-    width: initialWidth * 1000,
-    height: initialHeight * 1000,
-    position: 'relative',
-    top: '-50%',
-    left: '-50%',
-  });
+  var EmbeddedHelloWorld = getEmbeddedComponent(HelloWorld, [10000, 10000], document.body);
+  React.render(<EmbeddedHelloWorld/>, document.body);
 });
