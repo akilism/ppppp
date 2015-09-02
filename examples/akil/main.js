c283614f090dc1aa02a87c0304201210530425d2
Akil Harris <akil.harris@vice.com>
2015-09-01 20:27:33 -0400

    moved map into a react componenet. cleaned up adding and removing polyline path

--- examples/akil/main.js
+++ examples/akil/main.js
@@ -51,7 +51,7 @@ class TestComponent extends React.Component {
   render() {
     return (
       <div style={{position: 'relative', width: '100%', height: '100%'}}>
-        <Bg/>
+      
       </div>
     );
   }
@@ -112,43 +112,100 @@ class Bg extends ScanComponent {
   }
 }
 
-// class Map extends ScanComponent {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-
-//     }
-//   }
-// }
-
-var setMap = function(selector) {
-  var map = new google.maps.Map(document.getElementById(selector), {
-    center: {lat: 40.786858, lng: -73.962468},
-    zoom: 13
-  });
-
-  return map;
-}
-
-var getDirectionsPolyline = function(points) {
-  return new Promise(function(resolve, reject) {
-    var trip1 = {
-      origin: points[0],
-      destination: points[points.length-1],
-      travelMode: google.maps.TravelMode.WALKING
+class RouteMap extends ScanComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      routePoints: []
     };
+    TRV.scan_components.push(this);
+  }
 
-    var directions = new google.maps.DirectionsService();
-    directions.route(trip1, function(result, status) {
-      // console.log(result, status);
-      if(status !== "OK") {
-        reject(status);
-        return;
-      }
-      var polypoints = polyline.decode(result.routes[0].overview_polyline);
-      resolve(polypoints);
+  adjust(last_state, new_state) {
+    var marker_elapsed = new_state.markers["intro"](0).pct_elapsed;
+    var pointsIdx = Math.round(this.routes[0].routePoints.length * marker_elapsed);
+    console.log(marker_elapsed);
+    var that = this;
+    var poly = new google.maps.Polyline({
+      path: this.routes[0].routePoints.slice(0, pointsIdx).map(that.toLatLngObj),
+      strokeColor: '#000000',
+      strokeOpacity: 1.0,
+      strokeWeight: 3
     });
-  });
+    
+    poly.setMap(this.map);
+    if(this.routes[0].poly) { this.routes[0].poly.setMap(null); }
+    this.routes[0].poly = poly;
+    this.map.setCenter(this.toGoogleLatLng(this.routes[0].routePoints[pointsIdx]));
+    this.map.setZoom(15);
+    return { routePoints: this.routes[0].routePoints.slice(0, pointsIdx)};
+  }
+
+  toLatLngObj(point) { return {lat: point[0], lng: point[1]}; };
+  toGoogleLatLng(point) { return new google.maps.LatLng(point[0], point[1]); }
+
+  getDirectionsPolyline(points) {
+    var that = this;
+
+    return new Promise(function(resolve, reject) {
+      var trip = { origin: points[0],
+        waypoints: (points.length > 2) ? points.slice(1, destination.length) : [],
+        destination: points[points.length-1],
+        travelMode: google.maps.TravelMode.WALKING
+      };
+
+      var directions = new google.maps.DirectionsService();
+      directions.route(trip, function(result, status) {
+        if(status === "OK") { 
+          var routePoints = polyline.decode(result.routes[0].overview_polyline);
+          resolve(routePoints); 
+          return; 
+        }
+
+        reject(status); 
+      });
+    });
+  }
+
+  componentWillMount() {
+    this.routes = [{ poly: null,
+    markers: [{ marker: new google.maps.Marker({ position: { lat: 40.8025967, lng: -73.9502753}, 
+        animation: google.maps.Animation.DROP, 
+        title: 'Amy Ruth\'s'}),
+      trigger: null},
+      {marker: new google.maps.Marker({ position: { lat: 40.797814, lng: -73.960124}, 
+        animation: google.maps.Animation.DROP, 
+        title: 'Secret Smoke Spot'}),
+      trigger: null }]}];
+
+    var that = this;
+    return this.getDirectionsPolyline(this.routes[0].markers.map(function(m) {
+      return m.marker.position;
+    })).then(function(routePoints) {
+      that.routes[0].routePoints = routePoints;
+      that.state.routePoints = routePoints;
+    });
+  }
+
+  componentDidMount() {
+    this.map = new google.maps.Map(React.findDOMNode(this.refs.map), {
+      center: {lat: 40.786858, lng: -73.962468},
+      zoom: 14
+    });
+
+    var that = this;
+    _.forEach(this.routes, function(rte) {
+      _.forEach(_.where(rte.markers, {trigger: null}), function(m) {
+        m.marker.setMap(that.map);
+      });
+    });
+
+    this.map.setCenter(this.routes[0].markers[0].marker.position);
+  }
+
+  render() {
+    return (<div className="map" ref="map"></div>);
+  }
 }
 
 
@@ -156,57 +213,23 @@ $(function() {
   $("#page").height($(window).height() * 10);
   TRV.last_scroll = $(window).scrollTop();
   TRV.root = React.render(<TestComponent/>, document.getElementById('track'));
-
-  var map = setMap('map');
-  map.addListener('click', addLatLng);
-
-  function addLatLng(event) {
-  console.log(event);
-  // debugger;
-  }
-
-  var amyRuths = {lat: 40.8025967, lng: -73.9502753};
-  var amyRuthsMarker = new google.maps.Marker({position: amyRuths,
-    animation: google.maps.Animation.DROP,
-    title: 'Amy Ruth\'s'});
-  amyRuthsMarker.setMap(map);
-
-  var centralPark = {lat: 40.797814, lng: -73.960124};
-  var centralParkMarker = new google.maps.Marker({position: centralPark,
-    animation: google.maps.Animation.DROP,
-    title: 'Secret Smoke Spot'});
-  centralParkMarker.setMap(map);
-
-  getDirectionsPolyline([amyRuths, centralPark]).then(function(polyline) {
-    var poly = new google.maps.Polyline({
-      strokeColor: '#000000',
-      strokeOpacity: 1.0,
-      strokeWeight: 3
+  React.render(<RouteMap/>, document.getElementById('mapHolder'));
+  $(window).on("scroll",_.throttle(function(){
+    var new_scroll = $(window).scrollTop(),
+        window_height = $(window).height(),
+        $copy = $('#copy'),
+        new_copy_top = (new_scroll / window_height) * (- $copy.height() * 0.1);
+    var markers = TRV.getMarkers(new_copy_top, window_height);
+    $copy.css({
+      top: new_copy_top,
     });
-    poly.setMap(map);
-    var lastIdx = -1;
-
-    $(window).on("scroll",_.throttle(function(){
-      var new_scroll = $(window).scrollTop(),
-          window_height = $(window).height(),
-          $copy = $('#copy'),
-          new_copy_top = (new_scroll / window_height) * (- $copy.height() * 0.1);
-      var markers = TRV.getMarkers(new_copy_top, window_height);
-      var marker_elapsed = markers['intro'](0).pct_elapsed;
-      
-      var path = poly.getPath();
-      var pointsIdx = Math.round((polyline.length-1) * marker_elapsed);
-      var newPoint = polyline[pointsIdx];
-      console.log(lastIdx, pointsIdx);
-
-      if(pointsIdx > lastIdx) {
-        path.insertAt(pointsIdx, new google.maps.LatLng(newPoint[0], newPoint[1]));  
-      } else if (pointsIdx < lastIdx){
-        path.removeAt(pointsIdx);
-      }
-
-
-      lastIdx = pointsIdx;
-    },5));
-  });
+    _(TRV.scan_components).each(function(c){
+      var last_state = _(c.state).clone();
+      var new_state = c.adjust(last_state, {
+        scroll_top: new_scroll,
+        markers: markers,
+      });
+      c.setState(new_state);
+    });
+  },5));
 });
