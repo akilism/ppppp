9abbfaf6da81d706c4e80da64f2eb12d4e127801
Brian Kim <brian.kim@vice.com>
2015-09-02 23:43:46 -0400

    Check in shittily performing component

--- examples/infinite/main.js
+++ examples/infinite/main.js
@@ -1,94 +1,97 @@
-var $ = require('jquery');
-var React = require('react');
+window.$ = require('jquery');
+window._ = require('underscore');
+window.React = require('react');
+window.ReactDOM = require('react-dom');
+window.raf = require('raf');
 
 function prepareComponent(Component, viewport) {
-  class EmbeddableComponent extends React.Component {
+  class Embedder extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+
     getChildContext() {
-      return {
-        x: this.state.x,
-        y: this.state.y,
-        width: this.state.width,
-        height: this.state.height,
-      };
+      return this.state;
     }
 
     componentWillMount() {
+      $(viewport).empty();
       var width, height;
       if (viewport === document.body) {
         [width, height] = [$(window).width(), $(window).height()];
-      } else { 
+      } else {
         [width, height] = [$(viewport).width(), $(viewport).height()];
       }
+      if (!width || !height) {
+        throw new Error("Viewport must have a non-zero width and height");
+      }
+      $(viewport).css({
+        width,
+        height,
+        position: 'relative',
+        overflow: 'scroll',
+      });
       this.setState({width, height, x: 0, y: 0});
     }
 
-    render() {
-      if (!this.state.width || !this.state.height) {
-        throw new Error("Viewport must have an explicit width and height");
+    componentDidMount() {
+      if (viewport === document.body) {
+        $(window).on('scroll mousewheel', _.throttle(this.handleScroll.bind(this), 10));
+      } else {
+        $(viewport).on('scroll mousewheel', _.throttle(this.handleScroll.bind(this), 10));
       }
-      var style = {
-        width: this.state.width,
-        height: this.state.height,
-        position: 'relative',
-        overflow: 'scroll',
-        border: '1px solid black',
-      };
-      return (
-        <div style={style} onScroll={this.handleScroll.bind(this)}>
-          <Component />
-        </div>
-      );
+    }
+
+    componentWillUnmount() {
+      // TODO(brian)
+    }
+
+    render() {
+      return <Component />;
     }
 
     handleScroll(ev) {
-      var $target = $(ev.target);
-      var x = $target.scrollLeft();
-      var y = $target.scrollTop();
-      this.setState({x, y});
+      var x = $(ev.target).scrollLeft();
+      var y = $(ev.target).scrollTop();
+      this.setState({x,y});
     }
   }
 
-  EmbeddableComponent.childContextTypes = {
+  Embedder.childContextTypes = {
     width: React.PropTypes.number.isRequired,
     height: React.PropTypes.number.isRequired,
     x: React.PropTypes.number.isRequired,
     y: React.PropTypes.number.isRequired,
   };
 
-  return EmbeddableComponent;
+  return Embedder;
 }
 
 function embedComponent(Component, viewport, callback) {
-  $(viewport).empty();
   Component = prepareComponent(Component, viewport);
-  React.render(<Component/>, viewport, callback);
+  ReactDOM.render(<Component/>, viewport, callback);
 }
 
-
-class Infinite extends React.Component {
+class Root extends React.Component {
   render() {
-    console.log('infinite', this.context);
     return (
-      <div style={{height: 2000}}>
-        <Speed>
-          <Position/>
-        </Speed>
+      <div style={{height: this.context.height * 2}}>
+        <Position>
+          <div style={{backgroundColor: 'red', width: this.context.width, height: 300, border: '1px solid black'}}>
+          </div>
+        </Position>
       </div>
     );
   }
 }
 
-Infinite.contextTypes = {
-  y: React.PropTypes.number.isRequired,
+Root.contextTypes = {
+  width: React.PropTypes.number.isRequired,
+  height: React.PropTypes.number.isRequired,
 };
-
-//Infinite.childContextTypes = {
-//  y: React.PropTypes.number.isRequired,
-//};
 
 class Speed extends React.Component {
   render() {
-    console.log('speed', this.context);
     return this.props.children;
   }
 
@@ -102,20 +105,19 @@ class Speed extends React.Component {
 Speed.contextTypes = {
   y: React.PropTypes.number.isRequired,
 };
+
 Speed.childContextTypes = {
   y: React.PropTypes.number.isRequired,
 };
 
 class Position extends React.Component {
   render() {
-    console.log('position', this.context);
     var style = {
       position: 'absolute',
       top: this.context.y,
-      border: '1px solid black',
     };
     return (
-      <div style={style}>{this.context.y}</div>
+      <div style={style}>{this.props.children}</div>
     );
   }
 }
@@ -125,6 +127,5 @@ Position.contextTypes = {
 };
 
 $(function() {
-  var Infinite1 = prepareComponent(Infinite, document.body);
-  React.render(<Infinite1/>, document.body);
+  embedComponent(Root, document.body);
 });
