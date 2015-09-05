f18b57fc7603b9e065a0a868d2c4f0ba359746b6
Akil Harris <akil.harris@gmail.com>
2015-09-04 20:18:07 -0400

    Can add scan components into the content of an arc.

--- examples/akil3/main.js
+++ examples/akil3/main.js
@@ -84,10 +84,9 @@ class TestComponent extends React.Component {
   }
 
   render() {
-    var content = <img src="fireworks.jpg" style={{width: "100%"}} />;
     return (
       <div style={{position: 'relative', width: '100%', height: '100%'}}>
-        <TransitionComponent marker="crown-vic" anchor={0.5} content={content} />
+        <SoundTrigger marker="crown-vic" anchor="0.75" trigger="always" soundPath="../akil2/ambient_city.mp3" />
       </div>
     );
   }
@@ -208,7 +207,7 @@ class CopyComponent extends React.Component {
 
   render() {
     var arcs = this.props.arcs.map((a, i) => {
-      return <ArcComponent isActive={(a.name === this.state.activeArc)} setActive={this.setActive.bind(this, i)} ref={a.name} markers={a.markers} name={a.name} idx={i} key={a.name} />;
+      return <ArcComponent isActive={(a.name === this.state.activeArc)} setActive={this.setActive.bind(this, i)} ref={a.name} content={a.content} name={a.name} idx={i} key={a.name} />;
     })
    return (
       <div className="copy" ref="copy">
@@ -224,18 +223,31 @@ class ArcComponent extends React.Component {
   }
 
   handleClick(i) {
-    var marker = this.props.markers[i];
+    var marker = this.props.content[i];
     this.props.setActive(marker);
   }
 
+  getContent(content) {
+    return content.map((el, i) => {
+      switch (el.type) {
+        case "marker":
+          return <MarkerComponent isActive={this.props.isActive} clickHandler={this.handleClick.bind(this, i)} id={el.name} copy={el.copy} ref={el.name} key={el.name} idx={i} left={_.extend(el.left, {'direction': 'left'})} right={_.extend(el.right, {'direction': 'right'})} />;
+        case "transition":
+          return <TransitionComponent marker={el.marker} anchor={el.anchor} ref={el.name} key={el.name} />
+        case "timelapse":
+          return <Timelapse marker={el.marker} anchor={el.anchor} ref={el.name} key={el.name} />
+        default:
+          throw new Error("Unrecognized Component Type: " + el.type);
+      }
+    });
+  }
+
   render() {
-    var markers = this.props.markers.map((m, i) => {
-        return <MarkerComponent isActive={this.props.isActive} clickHandler={this.handleClick.bind(this, i)} id={m.name} copy={m.copy} ref={m.name} key={m.name} idx={i} left={_.extend(m.left, {'direction': 'left'})} right={_.extend(m.right, {'direction': 'right'})} />;
-      });
     var classes = "arc-" + this.props.idx + " " + this.props.name;
+    var content = this.getContent(this.props.content);
     return (
       <div className={classes}>
-        {markers}
+        {content}
       </div>
     );
   }
@@ -260,6 +272,7 @@ class TransitionComponent extends ScanComponent {
     super(props);
     this.state = {
       visible: false
+
     };
   }
 
@@ -268,7 +281,8 @@ class TransitionComponent extends ScanComponent {
     var marker_elapsed = marker.pct_elapsed;
 
     if(marker_elapsed > 0) {
-      return {visible: true};
+      var opacity = Math.linearTween(marker_elapsed, 0, 1, 1.0);
+      return {visible: true, opacity: opacity};
     }
 
     return {visible: false};
@@ -276,37 +290,172 @@ class TransitionComponent extends ScanComponent {
 
   getStyle() {
     if(this.state.visible) {
-      return {visibility: "visible", position: "relative",  top: "50%"};
+      return {visibility: "visible", opacity: this.state.opacity};
     } else {
-      return {visibility: "hidden"};
+      return {visibility: "visible", opacity: 0};
     }
   }
 
   render() {
+    var content = <img src="fireworks.jpg" style={{width: "100%"}} />;
     return (
       <div className="transition" style={this.getStyle()}>
-        {this.props.content}
+        {content}
       </div>
     );
   }
 }
 
+class Timelapse extends ScanComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+        frame: 0
+    };
+  }
+
+  adjust(last_state, d) {
+    var first_graf_elapsed = d.markers[this.props.marker](this.props.anchor).pct_elapsed,
+        frames = 59,
+        frame,bg_top;
+    frame = Math.round(frames * first_graf_elapsed);
+    return {frame: frame};
+  }
+
+  render() {
+    return(
+        <img style={{width: "100%"}} id="timelapse" src={"/erik/timelapse/frame_" + this.state.frame +".gif"}/>
+    )
+  }
+}
+
+class SoundTrigger extends ScanComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: false
+    }
+    // this.trigger = 'always';  //always, once, n plays
+    // this.soundPath = 'ambient_city.mp3';
+  }
+
+  loadAudioFile() {
+    console.log(this.props.soundPath);
+
+    return fetch(this.props.soundPath, {
+      method: "GET"
+    })
+    .then((response) => { return response.arrayBuffer(); })
+    .then((arrayBuff) => {
+      return new Promise((resolve, reject) => {
+        this.audioContext.decodeAudioData(arrayBuff,
+          function(buffer) { resolve(buffer); },
+          function(err) { reject(err); });
+      });
+    })
+    .catch((error) => { console.error(error); });
+  }
+
+  getWindowAudioContext() {
+    try {
+      window.AudioContext = window.AudioContext || window.webkitAudioContext;
+      window.audioctx = window.audioctx || new AudioContext();
+      return window.audioctx;
+    } catch (e) {
+      console.log('No Web Audio API support.');
+      return {};
+    }
+  }
+
+  getSource() {
+    if(!this.source) {
+      this.source = this.audioContext.createBufferSource();
+    }
+
+    return this.source;
+  }
+
+  play() {
+    if(!this.state.playing) {
+      // console.log("play");
+      var source = this.getSource();
+      source.buffer = this.soundBuffer;
+      source.connect(this.audioContext.destination);
+      source.loop = false;
+      source.start();
+    }
+  }
+
+  removeSource() { this.source = null; }
+
+  stop() {
+    if(this.source){
+      // console.log("stop");
+      var source = this.getSource();
+      source.stop();
+      this.removeSource();
+    }
+  }
+
+  adjust(last_state, d) {
+    var marker = d.markers[this.props.marker](this.props.anchor);
+    var marker_elapsed = marker.pct_elapsed;
+    // console.log("marker elapsed:", marker_elapsed);
+    if(marker.isActive) {
+      if(marker_elapsed > 0 && marker_elapsed < 0.25) {
+        this.play();
+        return {playing: true};
+      } else if(marker_elapsed === 1 || marker_elapsed === 0) {
+        this.stop();
+        return {playing: false};
+      }
+    }
+
+    return {playing: last_state.playing};
+  }
+
+  componentWillMount() {
+    this.audioContext = this.getWindowAudioContext();
+    this.loadAudioFile(this.audio).then((audioBuffer) => {
+      this.soundBuffer = audioBuffer;
+    });
+  }
+
+  render() {
+    return (
+      <p className="sound-trigger"></p>
+    );
+  }
+}
+
 var arcs = [{ "name": "Williamsburg",
-      "markers": [
+      "content": [
         { "name": "intro",
           "copy": "We just moved to a new office, which is in a different part of the neighborhood than we are used to. In New York, even something as small as a few blocks can change everything. To show us around, we hooked up with VICE veteran, Ben Kammerle, to show us all the spots.",
+          "type": "marker",
           "left": null,
           "right": null},
         { "name": "12-chairs",
           "copy": "He first took us to 12 chairs. One of the best new restaurants in the neighborhood and right next door to the office. It’s run by some badass Israelis - we talked to the owner and got his story. He ordered us some hummus and falafel. It was awesome but Ben was extremely rude to the staff. Ben was being a total jerk, but thank goodness 12 Chairs has some incredible mint lemonade to calm us down.",
+          "type": "marker",
           "left": null,
           "right": null},
+         { "name": "timelapse",
+          "type": "timelapse",
+          "marker": "crown-vic",
+          "anchor": 0.75},
         { "name": "crown-vic",
           "copy": "To Ben, Brooklyn in the summer means bars with backyards. \"Don’t even mess around with ones that don’t.\" he says, \"No one will be there.\" He took us to one of the best backyards, Crown Victoria. Probably because he’s cheap. In the back of the bar was a great taco truck... Great drinks... And tons of space to waste away your day. In brooklyn, day drinking is encouraged. There’s a tone of games to wile away the day. Cornholing. I don’t know why they call it that. Horseshoes. Not to mention, on sundays they roast an entire pig",
+          "type": "marker",
           "left": null,
           "right": null},
+        { "name": "fireworks-transition",
+          "type": "transition",
+          "marker": "crown-vic",
+          "anchor": 0.20},
         { "name": "crown-vic-2",
           "copy": "To Ben, Brooklyn in the summer means bars with backyards. \"Don’t even mess around with ones that don’t.\" he says, \"No one will be there.\" He took us to one of the best backyards, Crown Victoria. Probably because he’s cheap. In the back of the bar was a great taco truck... Great drinks... And tons of space to waste away your day. In brooklyn, day drinking is encouraged. There’s a tone of games to wile away the day. Cornholing. I don’t know why they call it that. Horseshoes. Not to mention, on sundays they roast an entire pig",
+          "type": "marker",
           "left": null,
           "right": null}
       ]}];
