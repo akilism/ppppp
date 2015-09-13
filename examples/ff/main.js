afd8692f4d4900e748575738c1352c94ac6d9123
Akil Harris <akil.harris@gmail.com>
2015-09-12 23:22:07 -0400

    basic progress bar.

--- examples/ff/main.js
+++ examples/ff/main.js
@@ -2,7 +2,7 @@ var $ = require('jquery');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var _ = require('underscore');
-var polyline = require('polyline');
+var directions = require('directions');
 
 Math.linearTween = function (t, b, c, d) {
   //
@@ -26,13 +26,28 @@ var scene = [
       "type": "title",
       "title": "MARSEILLE",
       "backgroundImage": "/erik3/marseille_1.jpg",
-      "fontSize": 250},
-    {"name": "progressmap",
-     "type": "routemap"},
+      "fontSize": 250,
+      "coords": { "lat": 40.8025967, "lng": -73.9502753},
+      "progressIndicator": {
+        "name": "Marseille",
+        "position": 0
+      }},
+    // {"name": "progressmap",
+    //  "type": "routemap"},
     {"name": "square",
-    "type": "slide1"},
+      "type": "slide1",
+      "coords": { "lat": 40.780916, "lng": -73.972981 },
+      "progressIndicator": {
+        "name": "Marseille Square",
+        "position": 0.25
+      }},
     {"name": "slide2",
-    "type": "slide2"},
+      "type": "slide2",
+      "coords": { "lat": 40.704021, "lng": -74.017073},
+      "progressIndicator": {
+        "name": "The Run Down",
+        "position": 0.45
+      }},
     // { "name": "intro",
     //   "copy": "We just moved to a new office, which is in a different part of the neighborhood than we are used to. In New York, even something as small as a few blocks can change everything. To show us around, we hooked up with VICE veteran, Ben Kammerle, to show us all the spots.",
     //   "type": "marker",
@@ -69,7 +84,7 @@ function createViewport(Component, container) {
       return {
         getPercentage: this.getPct.bind(this),
         getPercentageInverse: this.getInv.bind(this),
-        ...this.state
+        ...this.measurements
       };
     }
 
@@ -83,6 +98,11 @@ function createViewport(Component, container) {
       if (!viewportWidth || !viewportHeight) {
         throw new Error("Viewport must have a non-zero width and height");
       }
+
+      this.measurements = { viewportWidth, viewportHeight,
+        viewportLeft: 0, viewportTop: 0,
+        contentHeight: (viewportHeight * 8), adjustedViewportTop: 0,
+        pctScroll: 0};
 
       this.setState({
         viewportWidth, viewportHeight,
@@ -124,7 +144,9 @@ function createViewport(Component, container) {
           contentHeight = this.state.contentHeight,
           adjustedViewportTop = (viewportTop / this.state.viewportHeight) * (contentHeight * 0.1),
           pctScroll = viewportTop / (contentHeight - this.state.viewportHeight);
+      this.measurements = _.extend(this.measurements, {viewportLeft, viewportTop, contentHeight, adjustedViewportTop, pctScroll});
       this.setState({viewportLeft, viewportTop, contentHeight, adjustedViewportTop, pctScroll});
+      console.log("handleScroll: ", viewportTop, ':', pctScroll);
     }
 
     bfs(ref) {
@@ -160,6 +182,23 @@ function createViewport(Component, container) {
 }
 
 class Root extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      progressIndicators: []
+    }
+  }
+
+  componentWillMount() {
+    var progressIndicators = scene.filter((el) => {
+      return el.progressIndicator;
+    })
+    .map((el, i) => {
+      return _.extend(el.progressIndicator, {"coords": el.coords});
+    });
+    this.setState({progressIndicators: progressIndicators})
+  }
+
   buildComponents(content) {
     return content.map((el, i) => {
       switch (el.type) {
@@ -185,9 +224,12 @@ class Root extends React.Component {
 
   render() {
     var content = this.buildComponents(scene);
+    // console.log(this.state.progressIndicators);
+    console.log("root.render:", this.context.viewportTop, ':', this.context.pctScroll);
     return (
       <div>
         {content}
+        <ProgressBar ref="progressBar" progressIndicators={this.state.progressIndicators} />
       </div>
     );
   }
@@ -196,6 +238,7 @@ class Root extends React.Component {
 Root.contextTypes = {
   viewportHeight: React.PropTypes.number.isRequired,
   viewportTop: React.PropTypes.number.isRequired,
+  pctScroll: React.PropTypes.number.isRequired,
   viewportWidth: React.PropTypes.number.isRequired,
   adjustedViewportTop: React.PropTypes.number.isRequired,
 };
@@ -254,6 +297,7 @@ Base.contextTypes = {
   viewportTop: React.PropTypes.number.isRequired,
   adjustedViewportTop: React.PropTypes.number.isRequired,
   pctScroll: React.PropTypes.number.isRequired,
+  viewportWidth: React.PropTypes.number.isRequired
 };
 
 class MarkerComponent extends Base {
@@ -285,6 +329,7 @@ ScanComponent.contextTypes = {
   viewportHeight: React.PropTypes.number.isRequired,
   viewportTop: React.PropTypes.number.isRequired,
   contentHeight: React.PropTypes.number.isRequired,
+  viewportWidth: React.PropTypes.number.isRequired,
   pctScroll: React.PropTypes.number.isRequired,
   adjustedViewportTop: React.PropTypes.number.isRequired
 };
@@ -336,13 +381,20 @@ class Title extends ScanComponent {
   }
 
   componentWillReceiveProps() {
+    console.log('title.componentWillReceiveProps:', this.context.viewportTop, ':', this.context.pctScroll);
     this.setState(this.adjust(this.state));
+  }
+
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log('title.shouldComponentUpdate curr:', this.context.viewportTop, ':', this.context.pctScroll);
+    console.log('title.shouldComponentUpdate next:', nextContext.viewportTop, ':', nextContext.pctScroll);
+    return true;
   }
 
   adjust(last_state) {
     var {viewportHeight, viewportTop, adjustedViewportTop, contentHeight, pctScroll} = this.context;
 
-
+    // console.log('title.adjust:', viewportTop, ':', pctScroll);
     var dest_top = viewportHeight * -1 ;
     if(pctScroll < 0.1){
         // console.log("a",this.state.bg_top)
@@ -351,11 +403,13 @@ class Title extends ScanComponent {
         // console.log("t",this.state.bg_top)
         var new_top = dest_top;
     }
-    // return {bg_top: new_top, height: viewportHeight, width: "100%"};
-    return _.extend(last_state, {bg_top: new_top});
+    return {bg_top: new_top, height: viewportHeight, width: "100%"};
+    // this.setState(_.extend(last_state, {bg_top: new_top}));
+    return true;
   }
 
   render() {
+    // console.log('title.render:', this.context.viewportTop, ':', this.context.pctScroll);
     return(
       <div className='bg-slide' style={{
         // position: "absolute",
@@ -659,194 +713,63 @@ class Slide2 extends ScanComponent {
   }
 }
 
-class RouteMap extends ScanComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      routePoints: []
-    };
-  }
-
-  adjust(last_state) {
-    if(!this.routes[0].routePoints) { return last_state; }
-    var {viewportHeight, viewportTop, adjustedViewportTop, contentHeight, pctScroll} = this.context;
-    var pointsIdx = Math.round((this.routes[0].routePoints.length - 1) * pctScroll);
-    var currentPath = this.routes[0].routePoints.slice(0, pointsIdx);
-    var that = this;
-    var poly = new google.maps.Polyline({
-      path: currentPath.map(that.toLatLngObj),
-      strokeColor: '#000',
-      strokeOpacity: 1.0,
-      strokeWeight: 4
-    });
-
-    var point = this.routes[0].routePoints[pointsIdx];
-    if(point) {
-      var markers = this.getMarkerAt(point);
-      // console.log(this.routes[0].routePoints);
-      if(markers.length > 0) {
-        // console.log(markers);
-        if(markers[0].marker.added) {
-          markers[0].marker.setMap(null);
-          markers[0].marker.added = false;
-        } else {
-          markers[0].marker.setMap(this.map);
-          markers[0].marker.added = true;
-        }
-      }
-    }
-    poly.setMap(this.map);
-    if(this.routes[0].poly) { this.routes[0].poly.setMap(null); }
-    this.routes[0].poly = poly;
-
-    // this.map.setCenter(this.toGoogleLatLng(this.routes[0].routePoints[pointsIdx]));
-    // this.map.setZoom(15);
-
-    // var bounds = new google.maps.LatLngBounds(this.routes[0].markers[this.routes[0].markers.length-1].marker.position ,this.routes[0].markers[0].marker.position);
-    return _.extend(this.state, { routePoints: currentPath });
-  }
-
-  getMarkerAt(point) {
-    // return [];
-    // console.log("================================");
-    return this.routes[0].markers.filter(function(m) {
-      var isMarker = ((m.marker.position.G.toFixed(3) === point[0].toFixed(3)) && (m.marker.position.K.toFixed(3) === point[1].toFixed(3)));
-      // console.log('marker:', m.marker.position.G, m.marker.position.K);
-      // console.log('line point:', point[0], point[1]);
-      // console.log(isMarker);
-      // console.log("-----------------------------");
-      return isMarker;
-    });
-  }
-
-  getMarkersIn(path) {
-    return false;
-  }
-
-  toLatLngObj(point) { return {lat: point[0], lng: point[1]}; };
-  toGoogleLatLng(point) { return new google.maps.LatLng(point[0], point[1]); }
-  fromGoogleLatLng(point) { return [point.G, point.K]; }
-
-  addMidPoints(points, distThreshold, interpolationAmount) {
-    for(let len = points.length-1, i = 1; i < len; i++) {
-      let pointA = this.toGoogleLatLng(points[i-1]),
-        pointB = this.toGoogleLatLng(points[i]),
-        distanceBetween = google.maps.geometry.spherical.computeDistanceBetween(pointA, pointB);
-
-        if(distanceBetween >= distThreshold) {
-          let newPoint = this.fromGoogleLatLng(google.maps.geometry.spherical.interpolate(pointA, pointB, interpolationAmount));
-          // console.log("distGreater: ", distanceBetween, newPoint);
-          points.splice(i, 0, newPoint);
-        }
-    }
-    return points;
-  }
-
-  makeWaypoint(point) {
-    return {
-      location: point,
-      stopover: true
-    };
-  }
-
-  getDirectionsPolyline(points) {
-    // console.log(points);
-    return new Promise((resolve, reject) => {
-      var trip = { origin: points[0],
-        waypoints: (points.length > 2) ? points.slice(1, points.length).map(this.makeWaypoint) : [],
-        destination: points[points.length-1],
-        travelMode: google.maps.TravelMode.WALKING
-      };
-
-      var directions = new google.maps.DirectionsService();
-      directions.route(trip, (result, status) => {
-        if(status === "OK") {
-          console.log(result);
-          var routePoints = polyline.decode(result.routes[0].overview_polyline);
-          resolve(this.addMidPoints(routePoints, 50, 0.5));
-          return;
-        }
-        reject(status);
-      });
-    });
-  }
-
-  componentWillMount() {
-    this.routes = [{ poly: null,
-      markers: [{ marker: new google.maps.Marker({ position: { lat: 40.8025967, lng: -73.9502753},
-        animation: google.maps.Animation.DROP,
-        title: 'Amy Ruth\'s'}),
-      trigger: null},
-      {marker: new google.maps.Marker({ position: { lat: 40.797814, lng: -73.960124},
-        animation: google.maps.Animation.DROP,
-        title: 'Secret Smoke Spot'}),
-      trigger: null },
-      {marker: new google.maps.Marker({ position: { lat: 40.780916, lng: -73.972981},
-        animation: google.maps.Animation.DROP,
-        title: 'AMNH'}),
-      trigger: null },
-      {marker: new google.maps.Marker({ position: { lat: 40.783778, lng: -73.986376},
-        animation: google.maps.Animation.DROP,
-        title: 'Hudson River Hot Dog Vendor'}),
-      trigger: null },
-      {marker: new google.maps.Marker({ position: { lat: 40.704021, lng: -74.017073},
-        animation: google.maps.Animation.DROP,
-        title: 'Battery Park Underpass'}),
-      trigger: null }]}];
-
-    return this.getDirectionsPolyline(this.routes[0].markers.map((m) => {
-      return m.marker.position;
-    })).then((routePoints) => {
-      this.routes[0].routePoints = routePoints;
-      this.state.routePoints = routePoints;
-      var dimensions = {
-        width: 150,
-        height: this.context.viewportHeight,
-        mapHeight: this.context.viewportHeight - 10
-      };
-      // console.log(routePoints);
-      this.setState(_.extend(this.state, dimensions));
-    });
-  }
-
-  componentWillReceiveProps() { this.setState(this.adjust(this.state)); }
-
-  componentDidMount() {
-    var styles = [{ featureType: "all", stylers: [{ visibility: "off" }]}];
-    var mapNode = this.refs.map;
-
-    this.map = new google.maps.Map(this.refs.map, {
-      backgroundColor: 'transparent',
-      center: {lat: 40.786858, lng: -73.962468},
-      disableDefaultUI: true,
-      disableDoubleClickZoom: true,
-      draggable: false,
-      keyboardShortcuts: false,
-      scrollwheel: false,
-      zoom: 13
-    });
-    this.map.setOptions({styles: styles});
-
-    //Add all markers to map initially
-    // _.forEach(this.routes,(rte) => {
-    //   _.forEach(_.where(rte.markers, {trigger: null}), (m) => {
-    //     console.log(m);
-    //     m.marker.setMap(this.map);
-    //   });
-    // });
-
-    this.routes[0].markers[0].marker.setMap(this.map);
-    // this.map.setCenter(this.routes[0].markers[0].marker.position);
-    var bounds = new google.maps.LatLngBounds(this.routes[0].markers[this.routes[0].markers.length-1].marker.position ,this.routes[0].markers[0].marker.position);
-    // this.map.setCenter(bounds.getCenter());
-    this.map.fitBounds(bounds);
+class ProgressIndicator extends React.Component {
+  handleClick(ev) {
+    console.log(ev);
+    console.log(this.props);
   }
 
   render() {
     return (
-      <div className="mapHolder">
-        <div className="map" ref="map"></div>
-      </div>);
+      <circle cx={this.props.x} cy={this.props.y} r={this.props.r} onClick={this.handleClick.bind(this)} />
+    );
+  }
+}
+
+class ProgressBar extends ScanComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      progress: 0,
+      progressPx: 0,
+      progressPct: 0,
+      barWidth: 0,
+      barY: 125
+    };
+  }
+
+  componentWillMount() {
+    directions.getXY(this.props.progressIndicators).then((res) => {
+      console.log(res);
+      this.setState(_.extend(this.state, {barWidth: (this.context.viewportWidth - 80)}));
+    });
+  }
+
+  componentWillReceiveProps() {
+    this.setState(this.adjust(this.state));
+  }
+
+  adjust(last_state) {
+    var {viewportHeight, viewportTop, adjustedViewportTop, contentHeight, pctScroll, viewportWidth} = this.context;
+    return _.extend(last_state, {progressPx: Math.round(this.state.barWidth * pctScroll), progressPct: (pctScroll * 100)});
+  }
+
+  buildIndicator(i) {
+    var r = 4;
+    return <ProgressIndicator y={this.state.barY} x={(i.position * this.state.barWidth)+(r * 2)} r={r} name={i.name} key={i.name} />;
+  }
+
+  render() {
+    var indicators = _.map(this.props.progressIndicators, this.buildIndicator, this);
+    var pointsBar = [[0, this.state.barY], [this.state.barWidth, this.state.barY]];
+    var pointsFill = [[0, this.state.barY], [this.state.progressPx, this.state.barY]];
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" className="progress-svg" style={{width: (this.state.barWidth)}}>
+        <polyline className="progress-back" points={pointsBar} />
+        <polyline className="progress-fill" points={pointsFill} />
+        {indicators}
+      </svg>
+    );
   }
 }
 
@@ -857,7 +780,7 @@ function embedComponent(Component, container, callback) {
 }
 
 $(function() {
-  embedComponent(Root, document.querySelector("#app"));
+  embedComponent(Root, document.body);
 });
 
 var animatePov = function(sv,pov){
